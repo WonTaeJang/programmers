7307fdfc986fa722081158ef79722b3f78baabf9
WonTaeJang <jwt0212@gmail.com>
2022-05-22 22:06:22 +0900

    없는숫자 더하기

---
+++ Level1/없는숫자더하기.js
@@ -0,0 +1,23 @@
+function solution(numbers) {
+    var answer = 0;
+    const arr = new Array(10);
+    arr.fill(true);
+
+    numbers.map(e => {
+        arr[e] = false;
+    })
+
+    for(let i=0; i < 10; i++){
+        if(arr[i]){
+            answer += i;
+        }
+    }
+
+    console.log(answer);
+
+
+    return answer;
+}
+
+solution([1,2,3,4,6,7,8,0]);    // 14
+solution([5,8,4,0,6,7,9]);      // 6
