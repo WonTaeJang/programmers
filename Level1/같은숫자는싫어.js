ca090d265f35d0ed258bbb0fb253db6230514b8e
WonTaeJang <jwt0212@gmail.com>
2022-05-23 14:16:45 +0900

    같은숫자는 싫어

---
+++ Level1/같은숫자는싫어.js
@@ -0,0 +1,20 @@
+function solution(arr)
+{
+    var answer = [];
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    answer = arr.filter((number, index, target) => {
+        if(target[index+1] == number){
+            return false;
+        }
+        else
+            return true;
+    })
+    
+    console.log(answer)
+
+    return answer;
+}
+
+solution([1,1,3,3,0,1,1])   // [1,3,0,1]
+solution([4,4,4,3,3])       // [4,3]
