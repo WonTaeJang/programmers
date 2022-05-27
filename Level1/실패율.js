cec961b8b3562aa095525fa015c2ac5817174c52
WonTaeJang <jwt0212@gmail.com>
2022-05-27 12:30:09 +0900

    실패율

---
+++ Level1/실패율.js
@@ -0,0 +1,34 @@
+function solution(N, stages) {
+    var answer = [];
+
+    const failRate = new Array(N);
+    failRate.fill(0);
+
+    let cnt = stages.length;
+    for(let i=0; i<stages.length; i++){
+        if(stages[i] <= N)
+            failRate[stages[i]-1]++
+    }
+
+    // 실패율 계산
+    const failRate2 = failRate.map((value, i) => {
+        let result;
+        value === 0? result = 0 : result = value/cnt;
+        cnt -= value;
+
+        return {id:i, rate:result}
+    })
+
+    failRate2.sort((a,b)=>{
+        return b.rate-a.rate
+    })
+
+    for(value of failRate2){
+        answer.push(value.id+1);
+    }
+    console.log(answer)
+    return answer;
+}
+
+solution(5, [2, 1, 2, 6, 2, 4, 3, 3])   // [3,4,2,1,5]
+solution(4,	[4,4,4,4,4])    // [4,1,2,3]
