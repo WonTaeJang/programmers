a36b599505c6c1a73171095b14a1076ad86d7ab8
WonTaeJang <jwt0212@gmail.com>
2022-05-30 11:14:05 +0900

    서울에서 김서방 찾기

---
+++ Level1/서울에서김서방찾기.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+    var answer = '';
+
+    answer = seoul.indexOf('Kim');
+
+    console.log(answer);
+
+    return answer;
+}
+
+solution(["Jane", "Kim"]) //	"김서방은 1에 있다")
